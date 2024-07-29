83f82a58336d64285b806ea13ea6d028aa8ba2e8
joherulhoq <joherulhoq@gmail.com>
2024-07-29 22:29:18 +0600

    slider , product list, footer task complete

--- src/app/web/product-list/product-list.component.ts
+++ src/app/web/product-list/product-list.component.ts
@@ -13,28 +13,38 @@ import { ToastrService } from 'ngx-toastr';
 export class ProductListComponent {
   products = [
     {
-      title: 'Product 1',
+      stock : 'In Stock',
+      title: 'HP Laptop',
       price: 99.99,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      image: 'https://via.placeholder.com/300x200'
+      // image: 'https://via.placeholder.com/300x200',
+      image  : "assets/image/hp-laptop2.jpg"
     },
     {
+      stock : 'In Stock',
       title: 'Product 2',
       price: 149.99,
       description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      image: 'https://via.placeholder.com/300x200'
+      //image: 'https://via.placeholder.com/300x200'
+      image  : "assets/image/2.jpg"
     },
+    
+  
     {
-      title: 'Product 3',
-      price: 199.99,
-      description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
-      image: 'https://via.placeholder.com/300x200'
-    },
-    {
+      stock : 'In Stock',
       title: 'Product 4',
       price: 199.99,
       description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
-      image: 'https://via.placeholder.com/300x200'
+     // image: 'https://via.placeholder.com/300x200'
+      image  : "assets/image/5.jpg"
+    },
+    {
+      stock : 'In Stock',
+      title: 'Product 4',
+      price: 199.99,
+      description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+     // image: 'https://via.placeholder.com/300x200'
+      image  : "assets/image/17.jpg"
     }
   ];
 
