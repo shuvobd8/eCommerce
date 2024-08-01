49b9c3139c746b2da73c443f16675bf30dee8123
shafaetjsr <shafaet811@gmail.com>
2024-08-01 17:35:56 +0600

    session

--- src/app/web/product-list/product-list.component.ts
+++ src/app/web/product-list/product-list.component.ts
@@ -7,11 +7,12 @@ import { Observable } from 'rxjs';
 import { ApiResponseModel } from '../../core/classes/api-response.model';
 import { Product } from '../../core/classes/product';
 import { CartService } from '../../core/service/cart.service';
+import { SliderComponent } from '../slider/slider.component';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent,SliderComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
